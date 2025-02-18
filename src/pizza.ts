fc7e7657190ad25d6ee0a62c500937bb902cd46a
Alik <taezarlin1@gmail.com>
2025-02-18 21:12:24 +0630

    Void return type

--- src/pizza.ts
+++ src/pizza.ts
@@ -11,22 +11,24 @@ type Order = {
     status: "ordered" | "completed"
 }
 
-const menu: Pizza[] = [
-    { id: 1, name: "Margheriata", price: 8},
-    { id: 2, name: "Pepperoni", price: 10},
-    { id: 3, name: "Hawaiian", price: 10},
-    { id: 4, name: "Veggie", price: 9},
-]
-
 let cashInRegister = 100;
 let nextOrderId = 1;
+let nextPizzaId = 1;
+const menu: Pizza[] = [
+    { id: nextPizzaId++, name: "Margheriata", price: 8},
+    { id: nextPizzaId++, name: "Pepperoni", price: 10},
+    { id: nextPizzaId++, name: "Hawaiian", price: 10},
+    { id: nextPizzaId++, name: "Veggie", price: 9},
+]
+
+
 const orderQueue: Order[] = [];
 
-function addNewPizza(pizzaObj: Pizza){
-    menu.push(pizzaObj)
+function addNewPizza(pizzaObj: Pizza): void{
+    menu.push(pizzaObj);
 }
 
-function placeOrder(pizzaName: string){
+function placeOrder(pizzaName: string): Order | undefined{
     const selectedPizza = menu.find(pizzaObj=>pizzaObj.name === pizzaName)
     if(!selectedPizza){
         console.error(`${pizzaName} does not exist in the menu`)
@@ -49,7 +51,7 @@ export function getPizzaDetail(identifier: string | number): Pizza | undefined {
     }
 }
 
-function completeOrder(orderId: number){
+function completeOrder(orderId: number): Order | undefined{
     const order = orderQueue.find(order => order.id === orderId)
     if (!order) {
         console.error(`${orderId} was not found in the order queue`)
@@ -59,6 +61,6 @@ function completeOrder(orderId: number){
     return order
 }
 
-addNewPizza({ id: 5, name: "Chicken Ranch", price: 12})
-addNewPizza({ id: 6, name: "BBQ Chicken", price: 12})
-addNewPizza({ id: 7, name: "Spicy Sausage", price: 11})
+addNewPizza({ id: nextPizzaId++, name: "Chicken Ranch", price: 12})
+addNewPizza({ id: nextPizzaId++, name: "BBQ Chicken", price: 12})
+addNewPizza({ id: nextPizzaId++, name: "Spicy Sausage", price: 11})
